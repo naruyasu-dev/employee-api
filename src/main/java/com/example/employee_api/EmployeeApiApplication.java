3fbb3391d4f693aa9ecadc71fa9c85e905f1987e
ota <ota@ak-system>
2026-04-20 23:16:45 +0900

    tomcat 使用修正

--- src/main/java/com/example/employee_api/EmployeeApiApplication.java
+++ src/main/java/com/example/employee_api/EmployeeApiApplication.java
@@ -2,12 +2,20 @@ package com.example.employee_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+// 外部 Tomcat から起動できるようにする
 @SpringBootApplication
-public class EmployeeApiApplication {
+public class EmployeeApiApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeeApiApplication.class, args);
-	}
+	// Tomcat がこの Spring Boot アプリを見つけて起動するための設定
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EmployeeApiApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeApiApplication.class, args);
+    }
 }
