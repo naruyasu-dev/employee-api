497cc75aea903cea60cec9f62ca860af4648c544
ota <ota@ak-system>
2026-04-22 17:02:52 +0900

    Rest作成

---
+++ src/main/java/com/example/employee_api/EmployeeRestController.java
@@ -0,0 +1,23 @@
+package com.example.employee_api;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeRestController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeRestController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public List<Employee> findAll() {
+        return employeeService.findAll();
+    }
+}
