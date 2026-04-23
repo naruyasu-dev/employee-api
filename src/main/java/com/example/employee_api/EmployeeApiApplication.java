cf54bc20a4d622a9aab6318e1d6db955d5bdec0f
ota <nyaru@ak-system.jp>
2026-04-23 11:50:35 +0900

    111

--- src/main/java/com/example/employee_api/EmployeeApiApplication.java
+++ src/main/java/com/example/employee_api/EmployeeApiApplication.java
@@ -3,12 +3,19 @@ package com.example.employee_api;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-@MapperScan("com.example.employee_api")  // ★これが最重要
-public class EmployeeApiApplication {
+@MapperScan("com.example.employee_api")
+public class EmployeeApiApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeeApiApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EmployeeApiApplication.class);
     }
 }
