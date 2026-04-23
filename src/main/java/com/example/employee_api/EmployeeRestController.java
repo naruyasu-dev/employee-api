9f5fd6cc619976ed8580685d0302262bd93de911
ota <nyaru@ak-system.jp>
2026-04-23 10:41:17 +0900

    ff

--- src/main/java/com/example/employee_api/EmployeeRestController.java
+++ src/main/java/com/example/employee_api/EmployeeRestController.java
@@ -13,9 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/employees")
-@CrossOrigin(origins = "http://localhost:4200")
 public class EmployeeRestController {
 
     private final EmployeeService employeeService;
