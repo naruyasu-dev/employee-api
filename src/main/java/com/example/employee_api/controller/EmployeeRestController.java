5d2e82f6992996518dd32f290a776d122795b353
ota <nyaru@ak-system.jp>
2026-05-12 21:37:30 +0900

    fff

--- src/main/java/com/example/employee_api/controller/EmployeeRestController.java
+++ src/main/java/com/example/employee_api/controller/EmployeeRestController.java
@@ -2,7 +2,6 @@ package com.example.employee_api.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,8 +16,6 @@ import com.example.employee_api.service.EmployeeService;
 
 @RestController
 @RequestMapping("/employees")
-// @CrossOrigin(origins = "http://localhost:4200")
-@CrossOrigin(origins = "http://localhost:5173")
 public class EmployeeRestController {
 
     private final EmployeeService employeeService;
@@ -30,6 +27,11 @@ public class EmployeeRestController {
     @GetMapping
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/count")
+    public int count() {
+        return employeeService.count();
     }
 
     @GetMapping("/{id}")
